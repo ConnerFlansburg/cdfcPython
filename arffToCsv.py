6aa7e6046edb30ec4644bd83464cc4b3f55a75c1
brom94 <conner.flansburg@ou.edu>
2020-08-17 15:36:31 -0500

    attempted to get conversion to work on str

--- arffToCsv.py
+++ arffToCsv.py
@@ -3,30 +3,25 @@ import pandas as pd
 from tkinter import filedialog
 from scipy.io import arff
 
-# get the file path from a file browser
-tk.Tk().withdraw()  # prevent root window caused by Tkinter
+# the class column is the label column
+name = 'CLASS'  # + It should always be class, but check here for errors
+
+# *** Read in the .arff File as a Dataframe *** #
+tk.Tk().withdraw()                   # prevent root window caused by Tkinter
 path = filedialog.askopenfilename()  # prompt user for file path
+entries, __ = arff.loadarff(path)    # read in the .arff as a record array
+df = pd.DataFrame(entries)           # convert the record array into a dataframe
 
-# read in the .arff as a record array
-entries, __ = arff.loadarff(path)
+# *** Convert the CLASS Column if Needed *** #
+# ! label column will often need to be converted from an object BUT
+# ! what it will need to be converted to will change from dataset to dataset
+df["CLASS"] = df["CLASS"].astype(int)  # convert class id column to int from object
+# df["CLASS"] = df["CLASS"].astype(str)  # convert class id column to string from object
 
-# * used for debugging
-# pprint(meta)
+# *** Move the CLASS Column to be Where the CDFC Expects it to be  *** #
+first_col = df.pop(name)       # pop the class id column
+df.insert(0, name, first_col)  # insert the column back in at position 0
 
-# convert the record array into a dataframe
-df = pd.DataFrame(entries)
-
-# convert class id column to int from object
-df["CLASS"] = df["CLASS"].astype(int)
-
-# pop the class id column
-name = 'CLASS'
-first_col = df.pop(name)
-
-# insert the column back in at position 0
-df.insert(0, name, first_col)
-
-
-# export the dataframe as csv
+# *** Export the Dataframe as CSV *** #
 export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
 df.to_csv(export_file_path, index=False, encoding='utf-8')
